3bfe74660bc55bf0772415c909043f9550f7a18f
jostinOG <Jostinfabian14@gmail.com>
2022-12-19 16:07:52 +0100

    added nav

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"] # This is the default value
+ALLOWED_HOSTS = ['django-server-production-2d97.up.railway.app','127.0.0.1'] # Add your IP address here, to allow it to be accessed when testing
 
 # Application definition
 
